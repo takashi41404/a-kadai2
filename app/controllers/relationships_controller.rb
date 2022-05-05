a48cd2df1ec0720511600be7e5f43d420b1103ce
EC2 Default User <ec2-user@ip-172-31-13-135.ec2.internal>
2022-05-05 06:13:05 +0000

    follow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -6,7 +6,8 @@ class RelationshipsController < ApplicationController
 
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer  
+    redirect_to request.referer
+    
   end
   
   def followings
