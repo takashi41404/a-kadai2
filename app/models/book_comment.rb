8468542660bc010fdbb06e816776e41412df16e4
EC2 Default User <ec2-user@ip-172-31-13-135.ec2.internal>
2022-05-08 07:50:11 +0000

    kadai6-finish

--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -2,17 +2,19 @@ class BookComment < ApplicationRecord
   belongs_to :user
   belongs_to :book
   validates :comment, presence:true
-  def create
-    @book = book.find(params[:post_image_id])
-    @comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path
-  end
 
-  private
+  #↓なぜ記述したか不明のためコメントアウト
+  # def create
+  #   @book = book.find(params[:post_image_id])
+  #   @comment = current_user.book_comments.new(book_comment_params)
+  #   comment.book_id = book.id
+  #   comment.save
+  #   redirect_to book_path
+  # end
 
-  def book_comment_params
-    params.require(:book_comment).permit(:comment)
-  end
+  # private
+
+  # def book_comment_params
+  #   params.require(:book_comment).permit(:comment)
+  # end
 end
